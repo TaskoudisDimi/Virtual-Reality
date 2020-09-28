6bd5cda6ff65e6d6647da9fba4c2a4ba62e03604
Taskoudis Dimitris <63710755+Taskoudis-Dimi@users.noreply.github.com>
2020-09-28 15:29:13 +0300

    Add files via upload

---
+++ 2ndapp/Assets/Scenes/ButtonQuit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Numerics;
+
+public class ButtonQuit : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+
+
+
+
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        if (gvrTimer > totalTime)
+        {
+            GVRClick.Invoke();
+        }
+    }
+
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+
+    }
+
+
+}
